8a0e47a9a33e77aa2443e5bf2c8258eb105e1627
tadeas223 <94189582+tadeas223@users.noreply.github.com>
2025-11-23 22:40:31 +0100

    removed weird intelij thing
    Removed unnecessary tips for running code.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,8 +7,6 @@ import org.example.workplaces.Workplace;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         WorkTimer.setMinMax(1000, 2000);
